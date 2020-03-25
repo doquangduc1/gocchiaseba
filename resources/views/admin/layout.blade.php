4f080230e7924a267be7f6373aa88ba112c8717d
Đỗ Quang Đức <ducgets@gmail.com>
2020-03-26 02:53:18 +0700

    a

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -95,32 +95,31 @@
             <!-- Control sidebar content goes here -->
         </aside>
         <!-- /.control-sidebar -->
-    </div>
-            <!-- jQuery -->
-    <script type="text/javascript" src="{{ URL::asset('css/jquery/jquery.min.js') }}"></script>
-    <!-- Bootstrap 4 -->
-    <script  src="{{ URL::asset('css/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <!-- jsGrid -->
-    <script  src="{{ URL::asset('css/jsgrid/demos/db.js') }}"></script>
-    <script  src="{{ URL::asset('css/jsgrid/jsgrid.min.js') }}"></script>
-    <!-- AdminLTE App -->
-    <script  src="{{ URL::asset('css/dist/js/adminlte.min.js') }}"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script  src="{{ URL::asset('css/dist/js/demo.js') }}"></script>
-    <!-- page script -->
-</div>
+
+<!-- jQuery -->
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- DataTables -->
+<script src="../../plugins/datatables/jquery.dataTables.js"></script>
+<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+<!-- page script -->
 <script>
-    $(function () {
-        $("#example1").DataTable();
-        $('#example2').DataTable({
-            "paging": true,
-            "lengthChange": false,
-            "searching": false,
-            "ordering": true,
-            "info": true,
-            "autoWidth": false,
-        });
+  $(function () {
+    $("#example1").DataTable();
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
     });
+  });
 </script>
 
 </body>
