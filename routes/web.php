628bf91736e3b0d683e7912cdbc69d3a37f9edb2
Đỗ Quang Đức <ducgets@gmail.com>
2020-03-25 18:03:40 +0700

    a

--- routes/web.php
+++ routes/web.php
@@ -17,21 +17,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 //Route::get('admin.index', ['as' => 'admin.index', 'uses' => 'UserController@index']);
-routeAlias::group(['prefix'=>'admin'],function() {
-    routeAlias::get('admin.register', [ 'as' => 'register', 'uses' => 'UserController@register']);
+Route::group(['prefix'=>'admin'],function() {
+    Route::get('admin.register', [ 'as' => 'register', 'uses' => 'UserController@register']);
   //    Route::post('store',[ 'as' => 'store', 'uses' =>  'UserController@store']);
-    routeAlias::get('user.login',[ 'as'   => 'user.login', 'uses' => 'UserController@login']);
-      routeAlias::get('admin.layout',[ 'as' => 'admin.layout', 'uses' => 'UserController@layout']);
+    Route::get('user.login',[ 'as'   => 'user.login', 'uses' => 'UserController@login']);
+      Route::get('admin.layout',[ 'as' => 'admin.layout', 'uses' => 'UserController@layout']);
   //    route::post('login',[ 'as' => 'login', 'uses' => 'UserController@postlogin']);
   //    Route::get('logout',[ 'as' => 'logout', 'uses' => 'UserController@logout']);
-  //    routeAlias::resource('user', 'userController');
-      routeAlias::get('user.create',[ 'as' => 'user.create', 'uses' => 'UserController@create']);
-      routeAlias::get('user.index',[ 'as' => 'user.index', 'uses' => 'UserController@index']);
-      routeAlias::resource('products', 'ProductController');
+  //    Route::resource('user', 'userController');
+      Route::get('user.create',[ 'as' => 'user.create', 'uses' => 'UserController@create']);
+      Route::get('user.index',[ 'as' => 'user.index', 'uses' => 'UserController@index']);
+      Route::resource('products', 'ProductController');
   });
-  routeAlias::get('user.login',[ 'as'   => 'user.login', 'uses' => 'UserController@login']);
-   routeAlias::get('logout',[ 'as' => 'logout', 'uses' => 'UserController@logout']);
-   routeAlias::get('user.register',[ 'as'   => 'user.register', 'uses' => 'UserController@register']);
-   routeAlias::get('blog.index',[ 'as'   => 'blog.index', 'uses' => 'BlogController@index']);
-   routeAlias::get('products.create',[ 'as'   => 'products.create', 'uses' => 'ProductController@create']);
-   routeAlias::get('blog.add',[ 'as'   => 'blog.add', 'uses' => 'BlogController@add']);
+  Route::get('user.login',[ 'as'   => 'user.login', 'uses' => 'UserController@login']);
+   Route::get('logout',[ 'as' => 'logout', 'uses' => 'UserController@logout']);
+   Route::get('user.register',[ 'as'   => 'user.register', 'uses' => 'UserController@register']);
+   Route::get('blog.index',[ 'as'   => 'blog.index', 'uses' => 'BlogController@index']);
+   Route::get('products.create',[ 'as'   => 'products.create', 'uses' => 'ProductController@create']);
+   Route::get('blog.add',[ 'as'   => 'blog.add', 'uses' => 'BlogController@add']);
