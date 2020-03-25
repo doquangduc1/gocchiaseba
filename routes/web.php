b144743e802f95cef34807eb2582d280e1ea0c56
Đỗ Quang Đức <ducgets@gmail.com>
2020-03-25 18:52:11 +0700

    add

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.products.index');
 });
 //Route::get('admin.index', ['as' => 'admin.index', 'uses' => 'UserController@index']);
 Route::group(['prefix'=>'admin'],function() {
